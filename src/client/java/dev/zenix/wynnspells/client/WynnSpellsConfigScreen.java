2d88a78050d8a339b76e81cdc25d7396c2647152
OhhhZenix <ahom725@gmail.com>
2026-02-12 11:44:06 -0700

    Create WynnSpellsConfigScreen.java

---
+++ src/client/java/dev/zenix/wynnspells/client/WynnSpellsConfigScreen.java
@@ -0,0 +1,10 @@
+package dev.zenix.wynnspells.client;
+
+import net.minecraft.client.gui.screen.Screen;
+
+public class WynnSpellsConfigScreen {
+
+    public static Screen create(Screen parent) {
+        return parent;
+    }
+}
