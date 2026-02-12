81edfb302e3466b0eef2f20e1e6cbde95c4cb6ce
OhhhZenix <ahom725@gmail.com>
2026-02-12 11:17:30 -0700

    Create WynnSpellsConfig.java

---
+++ src/client/java/dev/zenix/wynnspells/client/WynnSpellsConfig.java
@@ -0,0 +1,15 @@
+package dev.zenix.wynnspells.client;
+
+public class WynnSpellsConfig {
+
+    private int delayMillis = 80;
+    private int queueLimit = 10;
+
+    public int getDelayMillis() {
+        return delayMillis;
+    }
+
+    public int getQueueLimit() {
+        return queueLimit;
+    }
+}
