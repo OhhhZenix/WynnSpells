61a3561fd8b353a528e369eacd2f0eefbd83fe0d
OhhhZenix <ahom725@gmail.com>
2026-02-17 10:07:29 -0700

    Update WynnSpellsRunnable.java

--- src/client/java/dev/zenix/wynnspells/client/WynnSpellsRunnable.java
+++ src/client/java/dev/zenix/wynnspells/client/WynnSpellsRunnable.java
@@ -4,11 +4,6 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.network.packet.c2s.play.HandSwingC2SPacket;
-import net.minecraft.network.packet.c2s.play.PlayerInputC2SPacket;
-import net.minecraft.network.packet.c2s.play.PlayerInteractItemC2SPacket;
-import net.minecraft.util.Hand;
-import net.minecraft.util.PlayerInput;
 
 public class WynnSpellsRunnable implements Runnable {
 
@@ -24,138 +19,104 @@ public class WynnSpellsRunnable implements Runnable {
     public void run() {
         while (running.get()) {
             try {
-                WynnSpellsQueue queue = queueList.take();
                 MinecraftClient client = MinecraftClient.getInstance();
-                if (client == null || client.player == null) {
-                    continue;
-                }
-
-                int delay = WynnSpellsClient.getInstance().getConfig().getDelayMillis();
-                // TODO: calculate delay based on ping
-
-                boolean queuedSneak = queue.isSneaking();
-                if (queuedSneak != client.player.isSneaking()) {
-                    sendSneakingPacket(client, queuedSneak);
-                    Thread.sleep(delay);
-                }
-
+                WynnSpellsQueue queue = queueList.take();
                 WynnSpellsIntent intent = queue.getIntent();
+
+                // TODO: calculate delay based on ping
+                int delay = WynnSpellsClient.getInstance().getConfig().getDelayMillis();
+
                 switch (intent) {
                     case MELEE:
                         if (WynnSpellsUtils.isArcher(client)) {
-                            sendInteractPacket(client);
+                            WynnSpellsUtils.sendInteractPacket(client);
                             Thread.sleep(delay);
                         } else {
-                            sendAttackPacket(client);
+                            WynnSpellsUtils.sendAttackPacket(client);
                             Thread.sleep(delay);
                         }
                         break;
                     case FIRST_SPELL:
                         if (WynnSpellsUtils.isArcher(client)) {
                             // L-R-L
-                            sendAttackPacket(client);
+                            WynnSpellsUtils.sendAttackPacket(client);
                             Thread.sleep(delay);
-                            sendInteractPacket(client);
+                            WynnSpellsUtils.sendInteractPacket(client);
                             Thread.sleep(delay);
-                            sendAttackPacket(client);
+                            WynnSpellsUtils.sendAttackPacket(client);
                             Thread.sleep(delay);
                         } else {
                             // R-L-R
-                            sendInteractPacket(client);
+                            WynnSpellsUtils.sendInteractPacket(client);
                             Thread.sleep(delay);
-                            sendAttackPacket(client);
+                            WynnSpellsUtils.sendAttackPacket(client);
                             Thread.sleep(delay);
-                            sendInteractPacket(client);
+                            WynnSpellsUtils.sendInteractPacket(client);
                             Thread.sleep(delay);
                         }
                         break;
                     case SECOND_SPELL:
                         if (WynnSpellsUtils.isArcher(client)) {
                             // L-L-L
-                            sendAttackPacket(client);
+                            WynnSpellsUtils.sendAttackPacket(client);
                             Thread.sleep(delay);
-                            sendAttackPacket(client);
+                            WynnSpellsUtils.sendAttackPacket(client);
                             Thread.sleep(delay);
-                            sendAttackPacket(client);
+                            WynnSpellsUtils.sendAttackPacket(client);
                             Thread.sleep(delay);
                         } else {
                             // R-R-R
-                            sendInteractPacket(client);
+                            WynnSpellsUtils.sendInteractPacket(client);
                             Thread.sleep(delay);
-                            sendInteractPacket(client);
+                            WynnSpellsUtils.sendInteractPacket(client);
                             Thread.sleep(delay);
-                            sendInteractPacket(client);
+                            WynnSpellsUtils.sendInteractPacket(client);
                             Thread.sleep(delay);
                         }
                         break;
                     case THIRD_SPELL:
                         if (WynnSpellsUtils.isArcher(client)) {
                             // L-R-R
-                            sendAttackPacket(client);
+                            WynnSpellsUtils.sendAttackPacket(client);
                             Thread.sleep(delay);
-                            sendInteractPacket(client);
+                            WynnSpellsUtils.sendInteractPacket(client);
                             Thread.sleep(delay);
-                            sendInteractPacket(client);
+                            WynnSpellsUtils.sendInteractPacket(client);
                             Thread.sleep(delay);
                         } else {
                             // R-L-L
-                            sendInteractPacket(client);
+                            WynnSpellsUtils.sendInteractPacket(client);
                             Thread.sleep(delay);
-                            sendAttackPacket(client);
+                            WynnSpellsUtils.sendAttackPacket(client);
                             Thread.sleep(delay);
-                            sendAttackPacket(client);
+                            WynnSpellsUtils.sendAttackPacket(client);
                             Thread.sleep(delay);
                         }
                         break;
                     case FOURTH_SPELL:
                         if (WynnSpellsUtils.isArcher(client)) {
                             // L-L-R
-                            sendAttackPacket(client);
+                            WynnSpellsUtils.sendAttackPacket(client);
                             Thread.sleep(delay);
-                            sendAttackPacket(client);
+                            WynnSpellsUtils.sendAttackPacket(client);
                             Thread.sleep(delay);
-                            sendInteractPacket(client);
+                            WynnSpellsUtils.sendInteractPacket(client);
                             Thread.sleep(delay);
                         } else {
                             // R-R-L
-                            sendInteractPacket(client);
+                            WynnSpellsUtils.sendInteractPacket(client);
                             Thread.sleep(delay);
-                            sendInteractPacket(client);
+                            WynnSpellsUtils.sendInteractPacket(client);
                             Thread.sleep(delay);
-                            sendAttackPacket(client);
+                            WynnSpellsUtils.sendAttackPacket(client);
                             Thread.sleep(delay);
                         }
                         break;
-                }
-
-                boolean physicalSneak = client.options.sneakKey.isPressed();
-                if (physicalSneak != client.player.isSneaking()) {
-                    sendSneakingPacket(client, physicalSneak);
-                    Thread.sleep(delay);
                 }
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 break;
             }
         }
-
-    }
-
-    private void sendAttackPacket(MinecraftClient client) {
-        WynnSpellsUtils.sendPacket(client, new HandSwingC2SPacket(Hand.MAIN_HAND));
-    }
-
-    private void sendInteractPacket(MinecraftClient client) {
-        WynnSpellsUtils.sendPacket(client, new PlayerInteractItemC2SPacket(Hand.MAIN_HAND, 0,
-                client.player.getYaw(), client.player.getPitch()));
-    }
-
-    private void sendSneakingPacket(MinecraftClient client, boolean isSneaking) {
-        PlayerInput playerInput = new PlayerInput(client.options.forwardKey.isPressed(),
-                client.options.backKey.isPressed(), client.options.leftKey.isPressed(),
-                client.options.rightKey.isPressed(), client.options.jumpKey.isPressed(), isSneaking,
-                client.options.sprintKey.isPressed());
-
-        WynnSpellsUtils.sendPacket(client, new PlayerInputC2SPacket(playerInput));
     }
 }
