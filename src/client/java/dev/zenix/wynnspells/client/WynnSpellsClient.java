0da22be90725d3a162e6899090a384aaa0fb240f
OhhhZenix <ahom725@gmail.com>
2026-02-17 10:03:46 -0700

    Update WynnSpellsClient.java

--- src/client/java/dev/zenix/wynnspells/client/WynnSpellsClient.java
+++ src/client/java/dev/zenix/wynnspells/client/WynnSpellsClient.java
@@ -1,14 +1,10 @@
 package dev.zenix.wynnspells.client;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.atomic.AtomicBoolean;
-
+import java.util.concurrent.LinkedBlockingDeque;
 import org.lwjgl.glfw.GLFW;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
@@ -21,37 +17,35 @@ import net.minecraft.util.Identifier;
 public class WynnSpellsClient implements ClientModInitializer {
 
     private static WynnSpellsClient instance = null;
-    private BlockingQueue<WynnSpellsQueue> queueList = new LinkedBlockingQueue<>();
-    private AtomicBoolean running = new AtomicBoolean(true);
     private WynnSpellsConfig config = null;
+
+    private LinkedBlockingDeque<WynnSpellsQueue> queueList = new LinkedBlockingDeque<>();
     private ItemStack previousItem = null;
-    private KeyBinding firstSpellKey;
-    private KeyBinding secondSpellKey;
-    private KeyBinding thirdSpellKey;
-    private KeyBinding fourthSpellKey;
-    private KeyBinding meleeKey;
-    private KeyBinding configKey;
+
+    private final Category KEY_CATEGORY = Category.create(Identifier.of("wynnspells", "all"));
+    private KeyBinding firstSpellKey =
+            KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynnspells.first",
+                    InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
+    private KeyBinding secondSpellKey =
+            KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynnspells.second",
+                    InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
+    private KeyBinding thirdSpellKey =
+            KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynnspells.third",
+                    InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
+    private KeyBinding fourthSpellKey =
+            KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynnspells.fourth",
+                    InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
+    private KeyBinding meleeKey =
+            KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynnspells.melee",
+                    InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
+    private KeyBinding configKey =
+            KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynnspells.config",
+                    InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
 
     @Override
     public void onInitializeClient() {
         instance = this;
         loadConfig();
-
-        var category = Category.create(Identifier.of("wynnspells", "all"));
-        firstSpellKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynnspells.first",
-                InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, category));
-        secondSpellKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynnspells.second",
-                InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, category));
-        thirdSpellKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynnspells.third",
-                InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, category));
-        fourthSpellKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynnspells.fourth",
-                InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, category));
-        meleeKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynnspells.melee",
-                InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, category));
-        configKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynnspells.config",
-                InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, category));
-        ClientLifecycleEvents.CLIENT_STARTED.register(client -> onClientStart(client));
-        ClientLifecycleEvents.CLIENT_STOPPING.register(client -> onClientStop(client));
         ClientTickEvents.END_CLIENT_TICK.register(client -> onClientEndTick(client));
     }
 
@@ -74,58 +68,39 @@ public class WynnSpellsClient implements ClientModInitializer {
         AutoConfig.getConfigHolder(WynnSpellsConfig.class).save();
     }
 
-    private void onClientStart(MinecraftClient client) {
-        Thread processorThread = new Thread(new WynnSpellsRunnable(queueList, running));
-        processorThread.setDaemon(true);
-        processorThread.start();
-    }
-
-    private void onClientStop(MinecraftClient client) {
-        running.set(false);
-    }
-
     private void onClientEndTick(MinecraftClient client) {
-        processIntentKey(firstSpellKey, WynnSpellsIntent.FIRST_SPELL, false);
-        processIntentKey(secondSpellKey, WynnSpellsIntent.SECOND_SPELL, false);
-        processIntentKey(thirdSpellKey, WynnSpellsIntent.THIRD_SPELL, false);
-        processIntentKey(fourthSpellKey, WynnSpellsIntent.FOURTH_SPELL, false);
-        processIntentKey(meleeKey, WynnSpellsIntent.MELEE, false);
         processConfigKey();
+        processIntentKey(client, firstSpellKey, WynnSpellsIntent.FIRST_SPELL);
+        processIntentKey(client, secondSpellKey, WynnSpellsIntent.SECOND_SPELL);
+        processIntentKey(client, thirdSpellKey, WynnSpellsIntent.THIRD_SPELL);
+        processIntentKey(client, fourthSpellKey, WynnSpellsIntent.FOURTH_SPELL);
+        processIntentKey(client, meleeKey, WynnSpellsIntent.MELEE);
+        processQueue(client);
     }
 
-    private void processIntentKey(KeyBinding key, WynnSpellsIntent intent, boolean repeatable) {
+    private void processIntentKey(MinecraftClient client, KeyBinding key, WynnSpellsIntent intent) {
         if (key == null)
             return;
 
         if (!key.isPressed())
             return;
 
-        if (!repeatable)
-            key.setPressed(false);
-
-        MinecraftClient client = MinecraftClient.getInstance();
-        if (client == null || client.options == null) {
-            return;
-        }
-
-        // does our player even exists?
-        if (client.player == null) {
-            return;
-        }
-
-        // if item different, lets clear it
-        ItemStack itemInMainHand = client.player.getMainHandStack();
-        if (itemInMainHand != previousItem) {
-            previousItem = itemInMainHand;
-            queueList.clear();
-        }
-
-        // should we keep casting?
         if (queueList.size() >= config.getQueueLimit()) {
             return;
         }
 
-        queueList.add(new WynnSpellsQueue(intent, client.options.sneakKey.isPressed()));
+        if (client == null || client.player == null) {
+            return;
+        }
+
+        ItemStack itemInMainHand = client.player.getMainHandStack();
+        if (itemInMainHand != previousItem) {
+            previousItem = itemInMainHand;
+            queueList.clear();;
+        }
+
+        key.setPressed(false);
+        queueList.add(new WynnSpellsQueue(intent));
     }
 
     private void processConfigKey() {
@@ -137,5 +112,79 @@ public class WynnSpellsClient implements ClientModInitializer {
 
         // TODO: Open config screen
         configKey.setPressed(false);
+    }
+
+    private void processQueue(MinecraftClient client) {
+        WynnSpellsQueue queue = queueList.poll();
+
+        if (queue == null)
+            return;
+
+        WynnSpellsIntent intent = queue.getIntent();
+        switch (intent) {
+            case MELEE:
+                if (WynnSpellsUtils.isArcher(client)) {
+                    WynnSpellsUtils.sendInteractPacket(client);
+                } else {
+                    WynnSpellsUtils.sendAttackPacket(client);
+                }
+                break;
+            case FIRST_SPELL:
+                if (WynnSpellsUtils.isArcher(client)) {
+                    // L-R-L
+                    WynnSpellsUtils.sendAttackPacket(client);
+                    WynnSpellsUtils.sendInteractPacket(client);
+                    WynnSpellsUtils.sendAttackPacket(client);
+
+                } else {
+                    // R-L-R
+                    WynnSpellsUtils.sendInteractPacket(client);
+                    WynnSpellsUtils.sendAttackPacket(client);
+                    WynnSpellsUtils.sendInteractPacket(client);
+
+                }
+                break;
+            case SECOND_SPELL:
+                if (WynnSpellsUtils.isArcher(client)) {
+                    // L-L-L
+                    WynnSpellsUtils.sendAttackPacket(client);
+                    WynnSpellsUtils.sendAttackPacket(client);
+                    WynnSpellsUtils.sendAttackPacket(client);
+
+                } else {
+                    // R-R-R
+                    WynnSpellsUtils.sendInteractPacket(client);
+                    WynnSpellsUtils.sendInteractPacket(client);
+                    WynnSpellsUtils.sendInteractPacket(client);
+                }
+                break;
+            case THIRD_SPELL:
+                if (WynnSpellsUtils.isArcher(client)) {
+                    // L-R-R
+                    WynnSpellsUtils.sendAttackPacket(client);
+                    WynnSpellsUtils.sendInteractPacket(client);
+                    WynnSpellsUtils.sendInteractPacket(client);
+                } else {
+                    // R-L-L
+                    WynnSpellsUtils.sendInteractPacket(client);
+                    WynnSpellsUtils.sendAttackPacket(client);
+                    WynnSpellsUtils.sendAttackPacket(client);
+                }
+                break;
+            case FOURTH_SPELL:
+                if (WynnSpellsUtils.isArcher(client)) {
+                    // L-L-R
+                    WynnSpellsUtils.sendAttackPacket(client);
+                    WynnSpellsUtils.sendAttackPacket(client);
+                    WynnSpellsUtils.sendInteractPacket(client);
+                } else {
+                    // R-R-L
+                    WynnSpellsUtils.sendInteractPacket(client);
+                    WynnSpellsUtils.sendInteractPacket(client);
+                    WynnSpellsUtils.sendAttackPacket(client);
+                }
+                break;
+        }
+
     }
 }
