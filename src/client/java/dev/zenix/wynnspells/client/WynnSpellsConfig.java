9f48238a619d2ce82fcfba65a0ebe86c8d47d930
OhhhZenix <ahom725@gmail.com>
2026-02-16 21:00:53 -0700

    Update WynnSpellsConfig.java

--- src/client/java/dev/zenix/wynnspells/client/WynnSpellsConfig.java
+++ src/client/java/dev/zenix/wynnspells/client/WynnSpellsConfig.java
@@ -6,7 +6,7 @@ import me.shedaniel.autoconfig.annotation.Config;
 @Config(name = "wynnspells")
 public class WynnSpellsConfig implements ConfigData {
 
-    private int delayMillis = 80;
+    private int delayMillis = 1000 * 20;
     private int queueLimit = 10;
 
     public int getDelayMillis() {
