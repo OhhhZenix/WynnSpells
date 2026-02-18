62bcc89a1c4c57a400bdde7622ddd87e0e7ee449
OhhhZenix <ahom725@gmail.com>
2026-02-17 18:13:59 -0700

    Update WynnSpellsClient.java

--- src/client/java/dev/zenix/wynnspells/client/WynnSpellsClient.java
+++ src/client/java/dev/zenix/wynnspells/client/WynnSpellsClient.java
@@ -22,6 +22,7 @@ import net.minecraft.util.Identifier;
 
 public class WynnSpellsClient implements ClientModInitializer {
 
+    public static final String MOD_NAME = "WynnSpells";
     public static final String MOD_ID = "wynnspells";
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
