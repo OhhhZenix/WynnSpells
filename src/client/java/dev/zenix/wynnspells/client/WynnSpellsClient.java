5fd65992aa94741cd7ae891dc9d54fa870162a65
OhhhZenix <ahom725@gmail.com>
2026-02-20 14:53:28 -0700

    Update WynnSpellsClient.java

--- src/client/java/dev/zenix/wynnspells/client/WynnSpellsClient.java
+++ src/client/java/dev/zenix/wynnspells/client/WynnSpellsClient.java
@@ -2,11 +2,6 @@ package dev.zenix.wynnspells.client;
 
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.atomic.AtomicBoolean;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.config.Configurator;
-import org.lwjgl.glfw.GLFW;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
@@ -20,6 +15,11 @@ import net.minecraft.client.util.InputUtil;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.lwjgl.glfw.GLFW;
 
 public class WynnSpellsClient implements ClientModInitializer {
 
@@ -31,37 +31,80 @@ public class WynnSpellsClient implements ClientModInitializer {
     private WynnSpellsConfig config = null;
 
     private AtomicBoolean running = new AtomicBoolean(true);
-    private LinkedBlockingDeque<WynnSpellsIntent> buffer = new LinkedBlockingDeque<>();
+    private LinkedBlockingDeque<WynnSpellsIntent> buffer =
+        new LinkedBlockingDeque<>();
     private ItemStack previousItem = null;
 
-    private final Category KEY_CATEGORY = Category.create(Identifier.of("wynnspells", "all"));
+    private final Category KEY_CATEGORY = Category.create(
+        Identifier.of("wynnspells", "all")
+    );
     private final KeyBinding FIRST_SPELL_KEY =
-            KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynnspells.first",
-                    InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
+        KeyBindingHelper.registerKeyBinding(
+            new KeyBinding(
+                "key.wynnspells.first",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_UNKNOWN,
+                KEY_CATEGORY
+            )
+        );
     private final KeyBinding SECOND_SPELL_KEY =
-            KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynnspells.second",
-                    InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
+        KeyBindingHelper.registerKeyBinding(
+            new KeyBinding(
+                "key.wynnspells.second",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_UNKNOWN,
+                KEY_CATEGORY
+            )
+        );
     private final KeyBinding THIRD_SPELL_KEY =
-            KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynnspells.third",
-                    InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
+        KeyBindingHelper.registerKeyBinding(
+            new KeyBinding(
+                "key.wynnspells.third",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_UNKNOWN,
+                KEY_CATEGORY
+            )
+        );
     private final KeyBinding FOURTH_SPELL_KEY =
-            KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynnspells.fourth",
-                    InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
-    private final KeyBinding MELEE_KEY =
-            KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynnspells.melee",
-                    InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
-    private final KeyBinding CONFIG_KEY =
-            KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynnspells.config",
-                    InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
+        KeyBindingHelper.registerKeyBinding(
+            new KeyBinding(
+                "key.wynnspells.fourth",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_UNKNOWN,
+                KEY_CATEGORY
+            )
+        );
+    private final KeyBinding MELEE_KEY = KeyBindingHelper.registerKeyBinding(
+        new KeyBinding(
+            "key.wynnspells.melee",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_UNKNOWN,
+            KEY_CATEGORY
+        )
+    );
+    private final KeyBinding CONFIG_KEY = KeyBindingHelper.registerKeyBinding(
+        new KeyBinding(
+            "key.wynnspells.config",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_UNKNOWN,
+            KEY_CATEGORY
+        )
+    );
 
     @Override
     public void onInitializeClient() {
         instance = this;
         setupLogger();
         loadConfig();
-        ClientLifecycleEvents.CLIENT_STARTED.register(client -> onClientStart(client));
-        ClientLifecycleEvents.CLIENT_STOPPING.register(client -> onClientStop(client));
-        ClientTickEvents.END_CLIENT_TICK.register(client -> onClientEndTick(client));
+        ClientLifecycleEvents.CLIENT_STARTED.register(client ->
+            onClientStart(client)
+        );
+        ClientLifecycleEvents.CLIENT_STOPPING.register(client ->
+            onClientStop(client)
+        );
+        ClientTickEvents.END_CLIENT_TICK.register(client ->
+            onClientEndTick(client)
+        );
     }
 
     public static WynnSpellsClient getInstance() {
@@ -89,40 +132,50 @@ public class WynnSpellsClient implements ClientModInitializer {
 
     private void onClientStart(MinecraftClient client) {
         WynnSpellsPingPong.start();
+        WynnSpellsUpdateChecker.start();
 
         Thread wynnSpells = new Thread(new WynnSpellsCaster(buffer, running));
         wynnSpells.setDaemon(true);
         wynnSpells.start();
-
-        Thread updateChecker = new Thread(new WynnSpellsUpdateChecker(running));
-        updateChecker.setDaemon(true);
-        updateChecker.start();
     }
 
     private void onClientStop(MinecraftClient client) {
         WynnSpellsPingPong.stop();
+        WynnSpellsUpdateChecker.stop();
         running.set(false);
     }
 
     private void onClientEndTick(MinecraftClient client) {
         processConfigKey(client);
         processIntentKey(client, FIRST_SPELL_KEY, WynnSpellsIntent.FIRST_SPELL);
-        processIntentKey(client, SECOND_SPELL_KEY, WynnSpellsIntent.SECOND_SPELL);
+        processIntentKey(
+            client,
+            SECOND_SPELL_KEY,
+            WynnSpellsIntent.SECOND_SPELL
+        );
         processIntentKey(client, THIRD_SPELL_KEY, WynnSpellsIntent.THIRD_SPELL);
-        processIntentKey(client, FOURTH_SPELL_KEY, WynnSpellsIntent.FOURTH_SPELL);
+        processIntentKey(
+            client,
+            FOURTH_SPELL_KEY,
+            WynnSpellsIntent.FOURTH_SPELL
+        );
         processIntentKey(client, MELEE_KEY, WynnSpellsIntent.MELEE);
     }
 
-    private void processIntentKey(MinecraftClient client, KeyBinding key, WynnSpellsIntent intent) {
-        if (key == null)
-            return;
+    private void processIntentKey(
+        MinecraftClient client,
+        KeyBinding key,
+        WynnSpellsIntent intent
+    ) {
+        if (key == null) return;
 
-        if (!key.isPressed())
-            return;
+        if (!key.isPressed()) return;
 
         if (buffer.size() >= config.getBufferLimit()) {
-            WynnSpellsUtils.sendNotification(Text.of("Cast ignored: spell queue is busy."),
-                    config.shouldNotifyBusyCast());
+            WynnSpellsUtils.sendNotification(
+                Text.of("Cast ignored: spell queue is busy."),
+                config.shouldNotifyBusyCast()
+            );
             return;
         }
 
@@ -141,11 +194,9 @@ public class WynnSpellsClient implements ClientModInitializer {
     }
 
     private void processConfigKey(MinecraftClient client) {
-        if (CONFIG_KEY == null)
-            return;
+        if (CONFIG_KEY == null) return;
 
-        if (!CONFIG_KEY.isPressed())
-            return;
+        if (!CONFIG_KEY.isPressed()) return;
 
         CONFIG_KEY.setPressed(false);
         client.setScreen(WynnSpellsConfigScreen.create(client.currentScreen));
