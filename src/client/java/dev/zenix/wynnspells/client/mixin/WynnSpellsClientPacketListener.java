9b47c065e2bd3d11841758d6dbc73eea82964702
OhhhZenix <ahom725@gmail.com>
2026-02-20 11:29:29 -0700

    Update WynnSpellsClientPacketListener.java

--- src/client/java/dev/zenix/wynnspells/client/mixin/WynnSpellsClientPacketListener.java
+++ src/client/java/dev/zenix/wynnspells/client/mixin/WynnSpellsClientPacketListener.java
@@ -2,18 +2,24 @@ package dev.zenix.wynnspells.client.mixin;
 
 import net.minecraft.client.network.PingMeasurer;
 import net.minecraft.network.packet.s2c.query.PingResultS2CPacket;
+import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import dev.zenix.wynnspells.client.WynnSpellsClient;
 import dev.zenix.wynnspells.client.WynnSpellsPingPong;
 
 @Mixin(PingMeasurer.class)
 public class WynnSpellsClientPacketListener {
 
-    @Inject(method = "onPingResult(Lnet/minecraft/network/packet/s2c/query/PingResultS2CPacket;)V",
-            at = @At("RETURN"))
-    private void onPingResultPost(PingResultS2CPacket packet, CallbackInfo ci) {
-        WynnSpellsPingPong.onCallback(packet.startTime());
+    @Inject(method = "onPingResult", at = @At("RETURN"))
+    private void handlePingResult(PingResultS2CPacket packet, CallbackInfo ci) {
+        long currentTime = Util.getMeasuringTimeMs();
+        long startTime = packet.startTime();
+        WynnSpellsClient.LOGGER.debug("Current Time: " + currentTime);
+        WynnSpellsClient.LOGGER.debug("Start Time: " + startTime);
+        WynnSpellsClient.LOGGER.debug("Ping: " + (currentTime - startTime));
+        WynnSpellsPingPong.onCallback(startTime);
     }
 }
