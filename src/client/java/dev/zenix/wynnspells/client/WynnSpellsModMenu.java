44e49e900a21dc4d6462d4193af5579b04b2016b
OhhhZenix <ahom725@gmail.com>
2026-02-22 21:15:05 -0700

    Update WynnSpellsModMenu.java

--- src/client/java/dev/zenix/wynnspells/client/WynnSpellsModMenu.java
+++ src/client/java/dev/zenix/wynnspells/client/WynnSpellsModMenu.java
@@ -5,8 +5,8 @@ import com.terraformersmc.modmenu.api.ModMenuApi;
 
 public class WynnSpellsModMenu implements ModMenuApi {
 
-    @Override
-    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> WynnSpellsConfigScreen.create(parent);
-    }
+	@Override
+	public ConfigScreenFactory<?> getModConfigScreenFactory() {
+		return parent -> WynnSpellsConfigScreen.create(parent);
+	}
 }
