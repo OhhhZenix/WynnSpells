acf65d383cf1787bc89ef48e49713e90f23302f9
OhhhZenix <ahom725@gmail.com>
2026-02-22 21:15:08 -0700

    Update WynnSpellsIntent.java

--- src/client/java/dev/zenix/wynnspells/client/WynnSpellsIntent.java
+++ src/client/java/dev/zenix/wynnspells/client/WynnSpellsIntent.java
@@ -1,9 +1,5 @@
 package dev.zenix.wynnspells.client;
 
 public enum WynnSpellsIntent {
-    MELEE,
-    FIRST_SPELL,
-    SECOND_SPELL,
-    THIRD_SPELL,
-    FOURTH_SPELL,
+	MELEE, FIRST_SPELL, SECOND_SPELL, THIRD_SPELL, FOURTH_SPELL,
 }
