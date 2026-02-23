c6f35d6b3c73841aa2e0604d44417b4a6c58039e
OhhhZenix <ahom725@gmail.com>
2026-02-23 10:33:06 -0700

    Update WynnSpellsIntent.java

--- src/client/java/dev/zenix/wynnspells/client/WynnSpellsIntent.java
+++ src/client/java/dev/zenix/wynnspells/client/WynnSpellsIntent.java
@@ -1,5 +1,19 @@
 package dev.zenix.wynnspells.client;
 
 public enum WynnSpellsIntent {
-	MELEE, FIRST_SPELL, SECOND_SPELL, THIRD_SPELL, FOURTH_SPELL,
+	MELEE, FIRST_SPELL, SECOND_SPELL, THIRD_SPELL, FOURTH_SPELL;
+
+	public boolean[] convert(boolean isArcher) {
+		return switch (this) {
+			case MELEE -> isArcher ? new boolean[]{true} : new boolean[]{false};
+
+			case FIRST_SPELL -> isArcher ? new boolean[]{false, true, false} : new boolean[]{true, false, true};
+
+			case SECOND_SPELL -> isArcher ? new boolean[]{false, false, false} : new boolean[]{true, true, true};
+
+			case THIRD_SPELL -> isArcher ? new boolean[]{false, true, true} : new boolean[]{true, false, false};
+
+			case FOURTH_SPELL -> isArcher ? new boolean[]{false, false, true} : new boolean[]{true, true, false};
+		};
+	}
 }
