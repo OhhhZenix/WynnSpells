a94d1de6e7ffa55bf63427af360001adcbcc19d7
OhhhZenix <ahom725@gmail.com>
2026-02-23 20:16:45 -0700

    Create SwingHandEvent.java

---
+++ src/client/java/dev/zenix/wynnspells/client/event/SwingHandEvent.java
@@ -0,0 +1,21 @@
+package dev.zenix.wynnspells.client.event;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+
+public interface SwingHandEvent {
+
+    Event<SwingHandEvent> EVENT = EventFactory.createArrayBacked(SwingHandEvent.class, (listeners) -> (player, hand) -> {
+        for (SwingHandEvent listener : listeners) {
+            return  listener.swingHand(player, hand);
+        }
+        return false;
+    });
+
+    // true == cancel
+    // false == not cancel
+    boolean swingHand(ClientPlayerEntity player, Hand hand);
+}
