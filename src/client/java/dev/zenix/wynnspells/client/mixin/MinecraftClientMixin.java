22bd0a17f98a948776f14f69d86f33267af1939b
OhhhZenix <ahom725@gmail.com>
2026-02-23 20:16:49 -0700

    Update MinecraftClientMixin.java

--- src/client/java/dev/zenix/wynnspells/client/mixin/MinecraftClientMixin.java
+++ src/client/java/dev/zenix/wynnspells/client/mixin/MinecraftClientMixin.java
@@ -1,6 +1,7 @@
 package dev.zenix.wynnspells.client.mixin;
 
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import dev.zenix.wynnspells.client.event.SwingHandEvent;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.util.Hand;
@@ -10,8 +11,9 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
 
-	@WrapWithCondition(method = "doAttack()Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;swingHand(Lnet/minecraft/util/Hand;)V"))
-	private boolean onStartAttack(ClientPlayerEntity player, Hand hand) {
-		return false;
-	}
+    @WrapWithCondition(method = "doAttack()Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;swingHand(Lnet/minecraft/util/Hand;)V"))
+    private boolean onSwingHand(ClientPlayerEntity player, Hand hand) {
+        boolean result = SwingHandEvent.EVENT.invoker().swingHand(player, hand);
+        return !result;
+    }
 }
