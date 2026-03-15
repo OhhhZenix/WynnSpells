e8739d69609d591f0c9d9dc8c30298daf844bea0
OhhhZenix <ahom725@gmail.com>
2026-03-14 19:07:27 -0600

    Create ClientPlayerInteractionManagerMixin.java

---
+++ src/client/java/dev/zenix/wynnspells/client/mixin/ClientPlayerInteractionManagerMixin.java
@@ -0,0 +1,27 @@
+package dev.zenix.wynnspells.client.mixin;
+
+import dev.zenix.wynnspells.client.event.InteractItemEvent;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ClientPlayerInteractionManager.class)
+public class ClientPlayerInteractionManagerMixin {
+
+	@Inject(method = "interactItem(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;)Lnet/minecraft/util/ActionResult;", at = @At("HEAD"), cancellable = true)
+	private void onUseItem(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+		boolean shouldCancel = InteractItemEvent.EVENT.invoker().interactItem(player, hand);
+
+		if (shouldCancel) {
+			cir.setReturnValue(ActionResult.CONSUME);
+		}
+
+		player.sendMessage(Text.of("Failed to cnacel"), false);
+	}
+}
