69489478cc4db9a1e7e0acdb3724f556ce259579
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2026-04-21 09:42:05 -0600

    Update PlayerInteractEvent.java

--- src/client/java/dev/zenix/wynnspells/client/event/PlayerInteractEvent.java
+++ src/client/java/dev/zenix/wynnspells/client/event/PlayerInteractEvent.java
@@ -8,13 +8,13 @@ import net.minecraft.world.entity.player.Player;
 
 public interface PlayerInteractEvent {
 
-    Event<PlayerInteractEvent> HANDLER = EventFactory.createArrayBacked(PlayerInteractEvent.class,
-            (listeners) -> (player, target, hand) -> {
-                for (PlayerInteractEvent callback : listeners) {
-                    return callback.interact(player, target, hand);
-                }
-                return false;
-            });
+	Event<PlayerInteractEvent> HANDLER = EventFactory.createArrayBacked(PlayerInteractEvent.class,
+			(listeners) -> (player, target, hand) -> {
+				for (PlayerInteractEvent callback : listeners) {
+					return callback.interact(player, target, hand);
+				}
+				return false;
+			});
 
-    boolean interact(Player player, Entity target, InteractionHand hand);
+	boolean interact(Player player, Entity target, InteractionHand hand);
 }
