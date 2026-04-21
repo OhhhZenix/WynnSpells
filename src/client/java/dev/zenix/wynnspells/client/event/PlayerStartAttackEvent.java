13182bd5492893deaf2301c3660bf78990edc190
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2026-04-21 09:30:46 -0600

    Create PlayerStartAttackEvent.java

---
+++ src/client/java/dev/zenix/wynnspells/client/event/PlayerStartAttackEvent.java
@@ -0,0 +1,19 @@
+package dev.zenix.wynnspells.client.event;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.InteractionHand;
+
+public interface PlayerStartAttackEvent {
+
+    Event<PlayerStartAttackEvent> HANDLER = EventFactory.createArrayBacked(PlayerStartAttackEvent.class,
+            (listeners) -> (player, hand) -> {
+                for (PlayerStartAttackEvent callback : listeners) {
+                    return callback.startAttack(player, hand);
+                }
+                return false;
+            });
+
+    boolean startAttack(LocalPlayer localPlayer, InteractionHand hand);
+}
