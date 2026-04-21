9d3a61823653bfaf4ad31e60244611c7f7229c09
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2026-04-20 23:43:08 -0600

    Create StartDestroyBlockEvent.java

---
+++ src/client/java/dev/zenix/wynnspells/client/event/StartDestroyBlockEvent.java
@@ -0,0 +1,21 @@
+package dev.zenix.wynnspells.client.event;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+
+public interface StartDestroyBlockEvent {
+
+	Event<StartDestroyBlockEvent> HANDLER = EventFactory.createArrayBacked(StartDestroyBlockEvent.class,
+			(listeners) -> (position, direction) -> {
+				for (StartDestroyBlockEvent callback : listeners) {
+					if (!callback.startDestroyBlock(position, direction)) {
+						return false;
+					}
+				}
+				return true;
+			});
+
+	boolean startDestroyBlock(BlockPos position, Direction direction);
+}
