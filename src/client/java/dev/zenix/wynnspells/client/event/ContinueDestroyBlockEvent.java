e52b33adf66cdcaa7a2a4f0406cc9d8295ed3d55
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2026-04-20 23:43:21 -0600

    Create ContinueDestroyBlockEvent.java

---
+++ src/client/java/dev/zenix/wynnspells/client/event/ContinueDestroyBlockEvent.java
@@ -0,0 +1,21 @@
+package dev.zenix.wynnspells.client.event;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+
+public interface ContinueDestroyBlockEvent {
+
+	Event<ContinueDestroyBlockEvent> HANDLER = EventFactory.createArrayBacked(ContinueDestroyBlockEvent.class,
+			(listeners) -> (position, direction) -> {
+				for (ContinueDestroyBlockEvent callback : listeners) {
+					if (!callback.continueDestroyBlock(position, direction)) {
+						return false;
+					}
+				}
+				return true;
+			});
+
+	boolean continueDestroyBlock(BlockPos position, Direction direction);
+}
