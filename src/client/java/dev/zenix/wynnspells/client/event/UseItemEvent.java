0d758ae1faa168ca861168e82efb175303766218
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2026-04-21 09:35:40 -0600

    Create UseItemEvent.java

---
+++ src/client/java/dev/zenix/wynnspells/client/event/UseItemEvent.java
@@ -0,0 +1,19 @@
+package dev.zenix.wynnspells.client.event;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+
+public interface UseItemEvent {
+
+    Event<UseItemEvent> HANDLER = EventFactory.createArrayBacked(UseItemEvent.class,
+            (listeners) -> (player, hand) -> {
+                for (UseItemEvent callback : listeners) {
+                    return callback.useItem(player, hand);
+                }
+                return false;
+            });
+
+    boolean useItem(Player player, InteractionHand hand);
+}
