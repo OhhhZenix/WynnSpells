93ec73b4aebdc752ed1357e98a024d278cb9e6af
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2026-04-21 09:39:25 -0600

    Create PlayerInteractEvent.java

---
+++ src/client/java/dev/zenix/wynnspells/client/event/PlayerInteractEvent.java
@@ -0,0 +1,20 @@
+package dev.zenix.wynnspells.client.event;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+
+public interface PlayerInteractEvent {
+
+    Event<PlayerInteractEvent> HANDLER = EventFactory.createArrayBacked(PlayerInteractEvent.class,
+            (listeners) -> (player, target, hand) -> {
+                for (PlayerInteractEvent callback : listeners) {
+                    return callback.interact(player, target, hand);
+                }
+                return false;
+            });
+
+    boolean interact(Player player, Entity target, InteractionHand hand);
+}
