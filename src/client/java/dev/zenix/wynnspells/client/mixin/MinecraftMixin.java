9263f35af14ffdd8e45cf415b68d379dfcc4b409
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2026-04-21 09:30:51 -0600

    Update MinecraftMixin.java

--- src/client/java/dev/zenix/wynnspells/client/mixin/MinecraftMixin.java
+++ src/client/java/dev/zenix/wynnspells/client/mixin/MinecraftMixin.java
@@ -1,6 +1,8 @@
 package dev.zenix.wynnspells.client.mixin;
 
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+
+import dev.zenix.wynnspells.client.event.PlayerStartAttackEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.InteractionHand;
@@ -11,8 +13,8 @@ import org.spongepowered.asm.mixin.injection.At;
 public class MinecraftMixin {
 
 	@WrapWithCondition(method = "startAttack()Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;swing(Lnet/minecraft/world/InteractionHand;)V"))
-	private boolean onAttack(LocalPlayer localPlayer, InteractionHand hand) {
-
-		return false;
+	private boolean startAttack(LocalPlayer localPlayer, InteractionHand hand) {
+		boolean result = PlayerStartAttackEvent.HANDLER.invoker().startAttack(localPlayer, hand);
+		return !result;
 	}
 }
