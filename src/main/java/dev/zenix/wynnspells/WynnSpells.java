4ba846321f933527392d10b64ad0f333434fb18a
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2026-04-20 23:42:01 -0600

    Update WynnSpells.java

--- src/main/java/dev/zenix/wynnspells/WynnSpells.java
+++ src/main/java/dev/zenix/wynnspells/WynnSpells.java
@@ -1,23 +1,17 @@
 package dev.zenix.wynnspells;
 
 import net.fabricmc.api.ModInitializer;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.config.Configurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WynnSpells implements ModInitializer {
 
 	public static final String MOD_NAME = "WynnSpells";
 	public static final String MOD_ID = "wynnspells";
-	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		setupLogger();
-	}
-
-	private void setupLogger() {
-		Configurator.setLevel(LOGGER.getName(), Level.INFO);
+		LOGGER.info("Hello Fabric world!");
 	}
 }
