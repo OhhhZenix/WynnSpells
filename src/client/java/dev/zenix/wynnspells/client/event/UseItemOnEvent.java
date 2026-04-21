0543b7f1950ae7a8be085e3edf7eb4935e913365
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2026-04-21 09:38:00 -0600

    Create UseItemOnEvent.java

---
+++ src/client/java/dev/zenix/wynnspells/client/event/UseItemOnEvent.java
@@ -0,0 +1,20 @@
+package dev.zenix.wynnspells.client.event;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.phys.BlockHitResult;
+
+public interface UseItemOnEvent {
+
+    Event<UseItemOnEvent> HANDLER = EventFactory.createArrayBacked(UseItemOnEvent.class,
+            (listeners) -> (player, hand, result) -> {
+                for (UseItemOnEvent callback : listeners) {
+                    return callback.useItemOn(player, hand, result);
+                }
+                return false;
+            });
+
+    boolean useItemOn(LocalPlayer player, InteractionHand hand, BlockHitResult result);
+}
