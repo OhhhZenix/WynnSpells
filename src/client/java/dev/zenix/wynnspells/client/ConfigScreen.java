aadec516b154c1ff9574a51f3399642985667f0a
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2026-05-15 12:39:23 -0600

    Update ConfigScreen.java

--- src/client/java/dev/zenix/wynnspells/client/ConfigScreen.java
+++ src/client/java/dev/zenix/wynnspells/client/ConfigScreen.java
@@ -1,9 +1,11 @@
 package dev.zenix.wynnspells.client;
 
+import com.mojang.blaze3d.platform.InputConstants;
+
+import dev.zenix.wynnspells.WynnSpells;
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
@@ -12,7 +14,7 @@ public class ConfigScreen {
 
 	public static Screen create(Screen parent) {
 		ConfigBuilder builder = ConfigBuilder.create().setParentScreen(parent)
-				.setTitle(Component.literal("WynnSpells"));
+				.setTitle(Component.nullToEmpty(WynnSpells.MOD_NAME));
 		ConfigEntryBuilder entryBuilder = builder.entryBuilder();
 
 		WynnSpellsClient wynnSpellsClient = WynnSpellsClient.getInstance();
@@ -20,61 +22,65 @@ public class ConfigScreen {
 		builder.setSavingRunnable(wynnSpellsClient::saveConfig);
 
 		// General
-		ConfigCategory generalCategory = builder.getOrCreateCategory(Component.literal("General"));
-		generalCategory.addEntry(
-				entryBuilder.startBooleanToggle(Component.literal("Notify Updates"), config.shouldNotifyUpdates())
-						.setTooltip(Component.literal("To enable or disable update notifications."))
+		ConfigCategory generalCategory = builder.getOrCreateCategory(Component.nullToEmpty("General"));
+		generalCategory
+				.addEntry(entryBuilder
+						.startBooleanToggle(Component.nullToEmpty("Notify Updates"), config.shouldNotifyUpdates())
+						.setTooltip(Component.nullToEmpty("To enable or disable update notifications."))
 						.setDefaultValue(ClothConfig.getDefaultNotifyUpdates())
 						.setSaveConsumer(config::setNotifyUpdates).build());
-		generalCategory.addEntry(
-				entryBuilder.startBooleanToggle(Component.literal("Notify Busy Cast"), config.shouldNotifyBusyCast())
-						.setTooltip(Component.literal("To enable or disable busy cast notifications."))
+		generalCategory
+				.addEntry(entryBuilder
+						.startBooleanToggle(Component.nullToEmpty("Notify Busy Cast"), config.shouldNotifyBusyCast())
+						.setTooltip(Component.nullToEmpty("To enable or disable busy cast notifications."))
 						.setDefaultValue(ClothConfig.getDefaultNotifyBusyCast())
 						.setSaveConsumer(config::setNotifyBusyCast).build());
 		generalCategory.addEntry(
-				entryBuilder.startBooleanToggle(Component.literal("Weapon Only"), config.isWeaponOnlyCasting())
-						.setTooltip(Component.literal("Allow casting keybinds only when a weapon is held."))
+				entryBuilder.startBooleanToggle(Component.nullToEmpty("Weapon Only"), config.isWeaponOnlyCasting())
+						.setTooltip(Component.nullToEmpty("Allow casting keybinds only when a weapon is held."))
 						.setDefaultValue(ClothConfig.getDefaultWeaponOnlyCasting())
 						.setSaveConsumer(config::setWeaponOnlyCasting).build());
 		generalCategory.addEntry(entryBuilder
-				.startBooleanToggle(Component.literal("Block Clicks"), config.getBlockClicks())
-				.setTooltip(Component.literal("Block left or right clicks while a spell is casting."))
+				.startBooleanToggle(Component.nullToEmpty("Block Clicks"), config.getBlockClicks())
+				.setTooltip(Component.nullToEmpty("Block left or right clicks while a spell is casting."))
 				.setDefaultValue(ClothConfig.getDefaultBlockClicks()).setSaveConsumer(config::setBlockClicks).build());
 		generalCategory.addEntry(
-				entryBuilder.startBooleanToggle(Component.literal("Use Auto Delay"), config.shouldUseAutoDelay())
-						.setTooltip(Component.literal("Automatically calculates the most optimal delay for you."))
+				entryBuilder.startBooleanToggle(Component.nullToEmpty("Use Auto Delay"), config.shouldUseAutoDelay())
+						.setTooltip(Component.nullToEmpty("Automatically calculates the most optimal delay for you."))
 						.setDefaultValue(ClothConfig.getDefaultUseAutoDelay()).setSaveConsumer(config::setUseAutoDelay)
 						.build());
-		generalCategory.addEntry(
-				entryBuilder.startBooleanToggle(Component.literal("Repeat Held Keys"), config.getRepeatHeldKeys())
-						.setTooltip(Component.literal("Allow action of a held key to be repeated."))
+		generalCategory
+				.addEntry(entryBuilder
+						.startBooleanToggle(Component.nullToEmpty("Repeat Held Keys"), config.getRepeatHeldKeys())
+						.setTooltip(Component.nullToEmpty("Allow action of a held key to be repeated."))
 						.setDefaultValue(ClothConfig.getDefaultRepeatHeldKeys())
 						.setSaveConsumer(config::setRepeatHeldKeys).build());
 		generalCategory.addEntry(entryBuilder
-				.startIntField(Component.literal("Auto Delay Tolerance"), config.getAutoDelayTolerance())
-				.setTooltip(Component
-						.literal("Milliseconds of error allowed per calculation. More is accurate. Less is faster."))
+				.startIntField(Component.nullToEmpty("Auto Delay Tolerance"), config.getAutoDelayTolerance())
+				.setTooltip(Component.nullToEmpty(
+						"Milliseconds of error allowed per calculation. More is accurate. Less is faster."))
 				.setDefaultValue(ClothConfig.getDefaultAutoDelayTolerance()).setSaveConsumer(value -> {
 					int clamped = Math.max(0, value);
 					config.setAutoDelayTolerance(clamped);
 				}).build());
-		generalCategory.addEntry(entryBuilder.startIntField(Component.literal("Manual Delay"), config.getManualDelay())
-				.setTooltip(
-						Component.literal("The delay between clicks. This value is ignored if auto delay is enabled."))
+		generalCategory.addEntry(entryBuilder
+				.startIntField(Component.nullToEmpty("Manual Delay"), config.getManualDelay())
+				.setTooltip(Component
+						.nullToEmpty("The delay between clicks. This value is ignored if auto delay is enabled."))
 				.setDefaultValue(ClothConfig.getDefaultManualDelay()).setSaveConsumer(value -> {
 					int clamped = Math.max(0, value);
 					config.setManualDelay(clamped);
 				}).build());
 		generalCategory.addEntry(entryBuilder
-				.startIntField(Component.literal("Repeat Threshold"), config.getRepeatThreshold())
-				.setTooltip(Component.literal(
+				.startIntField(Component.nullToEmpty("Repeat Threshold"), config.getRepeatThreshold())
+				.setTooltip(Component.nullToEmpty(
 						"The delay in milliseconds before the pressed key is counted as held key to repeat same action."))
 				.setDefaultValue(ClothConfig.getDefaultRepeatThreshold()).setSaveConsumer(value -> {
 					int clamped = Math.max(0, value);
 					config.setRepeatThreshold(clamped);
 				}).build());
 
-		ConfigCategory keybindsCategory = builder.getOrCreateCategory(Component.literal("Keybinds"));
+		ConfigCategory keybindsCategory = builder.getOrCreateCategory(Component.nullToEmpty("Keybinds"));
 		addKeybind(keybindsCategory, entryBuilder, WynnSpellsClient.CONFIG_KEY);
 		addKeybind(keybindsCategory, entryBuilder, WynnSpellsClient.MELEE_KEY);
 		addKeybind(keybindsCategory, entryBuilder, WynnSpellsClient.FIRST_SPELL_KEY);
@@ -88,9 +94,9 @@ public class ConfigScreen {
 	private static void addKeybind(ConfigCategory category, ConfigEntryBuilder entryBuilder, KeyMapping keyBinding) {
 		category.addEntry(entryBuilder
 				.startKeyCodeField(Component.translatable(keyBinding.getName()),
-						KeyBindingHelper.getBoundKeyOf(keyBinding))
-				.setTooltip(
-						Component.literal("Keybind for " + Component.translatable(keyBinding.getName()).getString()))
+						InputConstants.getKey(keyBinding.saveString()))
+				.setTooltip(Component
+						.nullToEmpty("Keybind for " + Component.translatable(keyBinding.getName()).getString()))
 				.setDefaultValue(keyBinding.getDefaultKey()).setKeySaveConsumer(value -> {
 					keyBinding.setKey(value);
 					Utils.refreshAndSaveKeyBindings();
