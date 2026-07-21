3eee890929b36bb842e97785fb46088df255e182
OhhhZenix <ahom725@gmail.com>
2026-07-21 15:08:34 -0600

    Update WynnSpellsClient.java

--- src/client/java/dev/zenix/wynnspells/client/WynnSpellsClient.java
+++ src/client/java/dev/zenix/wynnspells/client/WynnSpellsClient.java
@@ -10,122 +10,94 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.resources.Identifier;
-import net.minecraft.world.InteractionHand;
 import org.lwjgl.glfw.GLFW;
 
 public class WynnSpellsClient implements ClientModInitializer {
 
-    private static final KeyMapping.Category KEY_CATEGORY = KeyMapping.Category
-            .register(Identifier.fromNamespaceAndPath(WynnSpells.MOD_ID, "all"));
+	private static final KeyMapping.Category KEY_CATEGORY = KeyMapping.Category
+			.register(Identifier.fromNamespaceAndPath(WynnSpells.MOD_ID, "all"));
 
-    public static final KeyMapping FIRST_SPELL_KEY = KeyBindingHelper.registerKeyBinding(
-            new KeyMapping("key.wynnspells.first", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
+	public static final KeyMapping FIRST_SPELL_KEY = KeyBindingHelper.registerKeyBinding(
+			new KeyMapping("key.wynnspells.first", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
 
-    public static final KeyMapping SECOND_SPELL_KEY = KeyBindingHelper.registerKeyBinding(
-            new KeyMapping("key.wynnspells.second", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
+	public static final KeyMapping SECOND_SPELL_KEY = KeyBindingHelper.registerKeyBinding(
+			new KeyMapping("key.wynnspells.second", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
 
-    public static final KeyMapping THIRD_SPELL_KEY = KeyBindingHelper.registerKeyBinding(
-            new KeyMapping("key.wynnspells.third", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
+	public static final KeyMapping THIRD_SPELL_KEY = KeyBindingHelper.registerKeyBinding(
+			new KeyMapping("key.wynnspells.third", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
 
-    public static final KeyMapping FOURTH_SPELL_KEY = KeyBindingHelper.registerKeyBinding(
-            new KeyMapping("key.wynnspells.fourth", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
+	public static final KeyMapping FOURTH_SPELL_KEY = KeyBindingHelper.registerKeyBinding(
+			new KeyMapping("key.wynnspells.fourth", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
 
-    public static final KeyMapping MELEE_KEY = KeyBindingHelper.registerKeyBinding(
-            new KeyMapping("key.wynnspells.melee", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
+	public static final KeyMapping MELEE_KEY = KeyBindingHelper.registerKeyBinding(
+			new KeyMapping("key.wynnspells.melee", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
 
-    public static final KeyMapping CONFIG_KEY = KeyBindingHelper.registerKeyBinding(
-            new KeyMapping("key.wynnspells.config", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
+	public static final KeyMapping CONFIG_KEY = KeyBindingHelper.registerKeyBinding(
+			new KeyMapping("key.wynnspells.config", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, KEY_CATEGORY));
 
-    private static WynnSpellsClient instance = null;
-    private ClothConfig config;
-    private UpdateChecker updateChecker;
-    private PingTracker pingTracker;
-    private Caster caster;
+	private static WynnSpellsClient instance = null;
+	private ClothConfig config;
+	private UpdateChecker updateChecker;
+	private PingTracker pingTracker;
+	private Caster caster;
 
-    public static WynnSpellsClient getInstance() {
-        return instance;
-    }
+	public static WynnSpellsClient getInstance() {
+		return instance;
+	}
 
-    @Override
-    public void onInitializeClient() {
-        instance = this;
-        loadConfig();
-        ClientLifecycleEvents.CLIENT_STARTED.register(this::onClientStart);
-        ClientLifecycleEvents.CLIENT_STOPPING.register(this::onClientStop);
-        ClientTickEvents.END_CLIENT_TICK.register(this::onClientEndTick);
-    }
+	@Override
+	public void onInitializeClient() {
+		instance = this;
+		loadConfig();
+		ClientLifecycleEvents.CLIENT_STARTED.register(this::onClientStart);
+		ClientLifecycleEvents.CLIENT_STOPPING.register(this::onClientStop);
+		ClientTickEvents.END_CLIENT_TICK.register(this::onClientEndTick);
+	}
 
-    private void loadConfig() {
-        AutoConfig.register(ClothConfig.class, GsonConfigSerializer::new);
-        config = AutoConfig.getConfigHolder(ClothConfig.class).getConfig();
-        WynnSpells.LOGGER.info("Config loaded successfully");
-    }
+	private void loadConfig() {
+		AutoConfig.register(ClothConfig.class, GsonConfigSerializer::new);
+		config = AutoConfig.getConfigHolder(ClothConfig.class).getConfig();
+		WynnSpells.LOGGER.info("Config loaded successfully");
+	}
 
-    public ClothConfig getConfig() {
-        return config;
-    }
+	public ClothConfig getConfig() {
+		return config;
+	}
 
-    public void saveConfig() {
-        WynnSpells.LOGGER.debug("Saving configuration");
-        AutoConfig.getConfigHolder(ClothConfig.class).save();
-    }
+	public void saveConfig() {
+		WynnSpells.LOGGER.debug("Saving configuration");
+		AutoConfig.getConfigHolder(ClothConfig.class).save();
+	}
 
-    private void onClientStart(Minecraft client) {
-        updateChecker = new UpdateChecker();
-        updateChecker.start();
+	private void onClientStart(Minecraft client) {
+		updateChecker = new UpdateChecker();
+		updateChecker.start();
 
-        pingTracker = new PingTracker(client);
-        pingTracker.start();
+		pingTracker = new PingTracker(client);
+		pingTracker.start();
 
-        caster = new Caster(client);
-        caster.start();
-    }
+		caster = new Caster(client);
+		caster.start();
+	}
 
-    private void onClientStop(Minecraft client) {
-        updateChecker.stop();
-        pingTracker.stop();
-        caster.stop();
-    }
+	private void onClientStop(Minecraft client) {
+		updateChecker.stop();
+		pingTracker.stop();
+		caster.stop();
+	}
 
-    private void simulateLeftClick(Minecraft client) {
-        LocalPlayer player = client.player;
-        if (player == null || client.gameMode == null) {
-            return;
-        }
+	private void onClientEndTick(Minecraft client) {
+		processConfigKey(client);
+	}
 
-        client.gameMode.attack(player, null);
-        player.swing(InteractionHand.MAIN_HAND);
-    }
+	private void processConfigKey(Minecraft client) {
+		if (CONFIG_KEY.consumeClick()) {
+			client.setScreen(ConfigScreen.create(client.screen));
+		}
+	}
 
-    private void simulateRightClick(Minecraft client) {
-        LocalPlayer player = client.player;
-        if (player == null || client.gameMode == null) {
-            return;
-        }
-
-        client.gameMode.interact(player, null, InteractionHand.MAIN_HAND);
-        player.swing(InteractionHand.MAIN_HAND);
-    }
-
-    private void onClientEndTick(Minecraft client) {
-        // processConfigKey(client);
-
-        if (CONFIG_KEY.consumeClick() && client.player != null && client.gameMode != null) {
-            simulateRightClick(client);
-            simulateLeftClick(client);
-            simulateRightClick(client);
-        }
-    }
-
-    private void processConfigKey(Minecraft client) {
-        if (CONFIG_KEY.consumeClick()) {
-            client.setScreen(ConfigScreen.create(client.screen));
-        }
-    }
-
-    public PingTracker getPingTracker() {
-        return pingTracker;
-    }
+	public PingTracker getPingTracker() {
+		return pingTracker;
+	}
 }
