a8fab29f1107fa85a71b0fc72ede12bb0540ac80
OhhhZenix <ahom725@gmail.com>
2026-07-21 19:05:34 -0600

    Create ModConfig.java

---
+++ src/client/java/dev/zenix/wynnspells/client/config/ModConfig.java
@@ -0,0 +1,9 @@
+package dev.zenix.wynnspells.client.config;
+
+import dev.zenix.wynnspells.WynnSpells;
+import me.shedaniel.autoconfig.annotation.Config;
+
+@Config(name = WynnSpells.MOD_ID)
+public class ModConfig {
+
+}
