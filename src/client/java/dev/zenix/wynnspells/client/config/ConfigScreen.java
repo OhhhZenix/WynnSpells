ba49400cac46826acd97566bcc69d1433f2093a8
OhhhZenix <ahom725@gmail.com>
2026-07-21 19:05:32 -0600

    Create ConfigScreen.java

---
+++ src/client/java/dev/zenix/wynnspells/client/config/ConfigScreen.java
@@ -0,0 +1,10 @@
+package dev.zenix.wynnspells.client.config;
+
+import net.minecraft.client.gui.screens.Screen;
+
+public class ConfigScreen {
+
+    public static Screen create(Screen parent) {
+        return null;
+    }
+}
