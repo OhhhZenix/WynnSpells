54c911cda6519ac9aaa8d29eb0174c69efcc607d
OhhhZenix <ahom725@gmail.com>
2026-07-23 13:57:49 -0600

    Update Utils.java

--- src/client/java/dev/zenix/wynnspells/client/Utils.java
+++ src/client/java/dev/zenix/wynnspells/client/Utils.java
@@ -1,5 +1,56 @@
 package dev.zenix.wynnspells.client;
 
+import java.util.Map;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ServerboundSwingPacket;
+import net.minecraft.network.protocol.game.ServerboundUseItemPacket;
+import net.minecraft.world.InteractionHand;
+
 public class Utils {
 
+    enum Class {
+        WARRIOR,
+        MAGE,
+        ARCHER,
+        ASSASSIN,
+        SHAMAN,
+    }
+
+    public static final Map<Class, String> CLASS_ENCODINGS = Map.of(
+            Class.WARRIOR, "Warrior/Knight",
+            Class.MAGE, "Mage/Dark Wizard",
+            Class.ARCHER, "Archer/Hunter",
+            Class.ASSASSIN, "Assassin/Ninja",
+            Class.SHAMAN, "Shaman/Skyseer");
+
+    public static final Map<Class, String> WEAPON_ENCODINGS = Map.of(
+            Class.WARRIOR, "󐀂󐀁󏿿󏿿󏿿󏿿󏿿󏿿󏿠",
+            Class.MAGE, "󐀂󐀁󏿿󏿿󏿿󏿿󏿿󏿦",
+            Class.ARCHER, "󐀂󐀁󏿿󏿿󏿿󏿿󏿬",
+            Class.ASSASSIN, "󐀂󐀁󏿿󏿿󏿿󏿿󏿿󏿿󏿿󏿚",
+            Class.SHAMAN, "󐀂󐀁󏿿󏿿󏿿󏿿󏿿󏿿󏿢");
+
+    public static void sendPacket(Minecraft client, Packet<?> packet) {
+        if (client == null)
+            return;
+
+        ClientPacketListener networkHandler = client.getConnection();
+        if (networkHandler == null)
+            return;
+
+        networkHandler.send(packet);
+    }
+
+    public static void sendAttackPacket(Minecraft client) {
+        Utils.sendPacket(client, new ServerboundSwingPacket(InteractionHand.MAIN_HAND));
+    }
+
+    public static void sendInteractPacket(Minecraft client) {
+        float yrot = client.player.getYRot();
+        float xrot = client.player.getXRot();
+        Utils.sendPacket(client, new ServerboundUseItemPacket(InteractionHand.MAIN_HAND, 0, yrot, xrot));
+    }
 }
